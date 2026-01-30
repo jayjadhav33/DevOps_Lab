14a3f23d82369cc67cc0ec58447f47cc33dfdaab
Jay Jadhav <155727941+jayjadhav33@users.noreply.github.com>
2026-01-30 12:37:50 +0530

    Create code.cpp

---
+++ code.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Numbers 1 to 10:" << std::endl;
+    for (int i = 1; i <= 10; i++) {
+        std::cout << i << std::endl;
+    }
+    return 0;
+}
